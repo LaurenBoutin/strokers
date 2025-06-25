a29b481d329d9018b3eb40db8db43e59abcf8fec
Purploon <purploon@proton.me>
2025-06-25 20:09:32 +0200

    Ignore range in `normalised_from_funscript()` (#5)
    Also fixes returning a value out of range when `inverted` is true.
    
    The library's interpretation of the `range` value was wrong.
    Moreover, most script players seem to ignore this value.
    
    Ignoring the `range` therefore brings us into compatibility with other
    major script players.
    
    Reviewed-on: https://codeberg.org/LaurenBoutin/strokers/pulls/5
    Co-authored-by: Purploon <purploon@proton.me>
    Co-committed-by: Purploon <purploon@proton.me>

--- strokers_funscript/src/processing.rs
+++ strokers_funscript/src/processing.rs
@@ -15,20 +15,75 @@ pub struct NormalisedAction {
 pub fn normalised_from_funscript(funscript: &Funscript) -> Vec<NormalisedAction> {
     let mut out = Vec::with_capacity(funscript.actions.len());
 
-    let max_f64 = funscript.range as f64;
     let inverted = funscript.inverted;
 
     for action in &funscript.actions {
         let FunscriptAction { at, pos } = *action;
 
         let norm_pos = if inverted {
-            max_f64 * (1.0 - (pos as f64 / max_f64))
+            1.0 - (pos as f64 / 100.0)
         } else {
-            pos as f64 / max_f64
+            pos as f64 / 100.0
         } as f32;
 
         out.push(NormalisedAction { at, norm_pos });
     }
 
     out
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::json;
+
+    #[test]
+    fn test_normalised_from_funscript() {
+        macro_rules! genFunscriptTest {
+            ($inverted:expr, $range:expr, $pos:expr, $norm_pos_expected:expr) => {
+                FunscriptTest {
+                    funscript: Funscript {
+                        inverted: $inverted,
+                        range: $range,
+                        actions: Vec::from([FunscriptAction { at: 0, pos: $pos }]),
+                        unknown: json!(null),
+                    },
+                    norm_pos_expected: $norm_pos_expected,
+                }
+            };
+        }
+
+        struct FunscriptTest {
+            funscript: Funscript,
+            norm_pos_expected: f32,
+        }
+
+        let tests = Vec::from([
+            // Inverted false and range 100
+            genFunscriptTest!(false, 100, 0, 0.0),
+            genFunscriptTest!(false, 100, 100, 1.0),
+            genFunscriptTest!(false, 100, 50, 0.5),
+            genFunscriptTest!(false, 100, 25, 0.25),
+            // Inverted true and range 100
+            genFunscriptTest!(true, 100, 0, 1.0),
+            genFunscriptTest!(true, 100, 100, 0.0),
+            genFunscriptTest!(true, 100, 50, 0.5),
+            genFunscriptTest!(true, 100, 25, 0.75),
+            // Inverted false and range 90
+            genFunscriptTest!(false, 90, 0, 0.0),
+            genFunscriptTest!(false, 90, 100, 1.0),
+            genFunscriptTest!(false, 90, 50, 0.5),
+            genFunscriptTest!(false, 90, 25, 0.25),
+            // Inverted true and range 90
+            genFunscriptTest!(true, 90, 0, 1.0),
+            genFunscriptTest!(true, 90, 100, 0.0),
+            genFunscriptTest!(true, 90, 50, 0.5),
+            genFunscriptTest!(true, 90, 25, 0.75),
+        ]);
+
+        for test in tests.iter() {
+            let result = normalised_from_funscript(&test.funscript);
+            assert_eq!(test.norm_pos_expected, result[0].norm_pos);
+        }
+    }
 }
